f24eac00e316b01f3b188f6c98112b4ca4460ed8
Silvia Reyes <104657695+NathaliaReyes@users.noreply.github.com>
2024-07-22 17:18:35 -0600

    Fixing Car Model

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -17,6 +17,8 @@ const typeDefs = `
     price: Int!
     color: String!
     mileage: Int!
+    description: String
+    images: [String]
     created_at: Date
     updated_at: Date
   }
@@ -32,9 +34,9 @@ const typeDefs = `
     
   type Mutation {
     addUser(username: String!, email: String!, password: String!): Auth
-    addCar(make: String!, model: String!, year: Int!, price: Int!, color: String!, mileage: Int!, created_at: Date, updated_at: Date): Car
+    addCar(make: String!, model: String!, year: Int!, price: Int!, color: String!, mileage: Int!, created_at: Date, updated_at: Date, description: String, images: [String]): Car
     login(email: String!, password: String!): Auth
-    updateCar(_id: ID!, make: String, model: String, year: Int, price: Int, color: String, mileage: Int): Car
+    updateCar(_id: ID!, make: String, model: String, year: Int, price: Int, color: String, mileage: Int, description: String,  images: [String]): Car
     deleteCar(_id: ID!): Car
   }
 `;
