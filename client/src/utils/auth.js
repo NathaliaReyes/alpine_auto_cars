c9321935dfe3350da9d0b144f70eaed2aad7e595
Silvia Reyes <104657695+NathaliaReyes@users.noreply.github.com>
2024-07-26 17:12:22 -0600

    List of Clients added after loggedIn the Admin can see the list from the Admin Tools

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -43,8 +43,8 @@ class AuthService {
   logout() {
     // Clear user token and profile data from localStorage
     localStorage.removeItem('id_token');
-    // this will reload the page and reset the state of the application
-    window.location.reload('/');
+    // this will reload the page and send to the home page
+    window.location.assign('/');
   }
 }
 
