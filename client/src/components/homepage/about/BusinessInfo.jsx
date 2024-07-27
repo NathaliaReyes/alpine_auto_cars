72805856517c026d74b52e6dafef2fc9c3f79c71
Silvia Reyes <104657695+NathaliaReyes@users.noreply.github.com>
2024-07-27 13:20:26 -0600

    alert for login authorization in the Admin Login

--- client/src/components/homepage/about/BusinessInfo.jsx
+++ client/src/components/homepage/about/BusinessInfo.jsx
@@ -14,11 +14,11 @@ function BusinessInfo() {
       </div>
       <div className="mb-4 lg:mb-0 text-white">
         <div className="flex flex-col font-semibold">
-          <a href="#link1" className="flex">
+          <a href="tel:+19704264356" className="flex">
             <PhoneIcon className="w-5 h-5 mr-2 text-red-600 mb-2" />
             970-426-4356
           </a>
-          <a href="#link2" className="flex">
+          <a href="https://www.google.com/maps/dir//Alpine+Auto+Sales+LLC+828+Co+Rd+501+Bayfield,+CO+81122/@37.2430787,-107.6013486,16z/data=!4m5!4m4!1m0!1m2!1m1!1s0x873c21adba3328e5:0xa17770b3fcd18eb9" target="_blank" rel="noopener noreferrer" className="flex">
             <LocationMarkerIcon className="w-5 h-5 mr-2 text-red-600" />
             828 Co Rd 501 <br />Bayfield, CO 81122
           </a>
@@ -29,8 +29,8 @@ function BusinessInfo() {
         <div className="flex font-semibold">
           <ClockIcon className="w-5 h-5 mr-2 text-red-600" />
           <div className="flex flex-col ">
-            <a href="#link3">Monday - Friday 8:00 - 17:00</a>
-            <a href="#link4">Saturday - By Appt Only</a>
+            <a href="tel:+19704264356">Monday - Friday 8:00 - 17:00</a>
+            <a href="tel:+19704264356">Saturday - By Appt Only</a>
           </div>
         </div>
       </div>
