cc5546af20a89046a18d51bd0182d0b8c6db7f29
Silvia Reyes <104657695+NathaliaReyes@users.noreply.github.com>
2024-08-12 14:19:48 -0600

    AOS animation library added instead of the countUp, component slogan added

---
+++ client/src/components/slogan/Slogan.jsx
@@ -0,0 +1,73 @@
+import React, { useEffect } from "react";
+import { FaHandshake, FaCar, FaShieldAlt, FaStar, FaThumbsUp } from 'react-icons/fa';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+export default function Slogan() {
+  useEffect(() => {
+    AOS.init({
+      duration: 1500,
+    });
+  }, []);
+
+  return (
+    <div className="mx-3">
+      <div>
+        <p className="text-base md:text-lg text-black font-semibold text-center p-2">
+          Your Trusted Partner in Car Deals <span className="inline-block"><FaHandshake /></span>
+        </p>
+      </div>
+      <div className="my-4 rounded border-customWhite border-2 p-4">
+        <div className="grid sm:grid-cols-2 lg:grid-cols-4 gap-4 justify-items-center">
+          {/* Buy */}
+          <div 
+            className="flex flex-col items-center"
+            data-aos="zoom-in-up"
+            >
+            <FaCar className="text-red-600 text-6xl sm:text-7xl md:text-8xl mt-3" />
+            <div className="flex items-center">
+              <span className="text-black font-bold xl-text">Buy</span>
+            </div>
+            <p className="text-muted mt-1">Quality Guaranteed</p>
+          </div>
+
+          {/* Sell */}
+          <div 
+            className="flex flex-col items-center"
+            data-aos="zoom-in-up"
+            >
+            <FaStar className="text-red-600 text-6xl sm:text-7xl md:text-8xl mt-3" />
+            <div className="flex items-center">
+              <span className="text-black font-bold xl-text">Sell</span>
+            </div>
+            <p className="text-neutral">Exceptional Deals</p>
+          </div>
+
+          {/* Trade */}
+          <div 
+            className="flex flex-col items-center"
+            data-aos="zoom-in-up"
+            >
+            <FaShieldAlt className="text-red-600 text-6xl sm:text-7xl md:text-8xl mt-3"/>
+            <div className="flex items-center">
+              <span className="text-black font-bold xl-text">Trade</span>
+            </div>
+            <p className="text-neutral">Fair Value</p>
+          </div>
+
+          {/* Find */}
+          <div 
+            className="flex flex-col items-center"
+            data-aos="zoom-in-up"
+            >
+            <FaThumbsUp className="text-red-600 text-6xl sm:text-7xl md:text-8xl mt-3" />
+            <div className="flex items-center">
+              <span className="text-black font-bold xl-text">Find</span>
+            </div>
+            <p className="text-neutral">Your Ideal Car</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,6 +5,7 @@ import { Button } from '@/components/ui/button';
 import { BackgroundBeams } from '@/components/ui/background-beams';
 // import LoginForm from '@/screens/LoginForm';
 import AboutUs from '@/components/homepage/about/AboutUs';
+import Slogan from '@/components/slogan/Slogan';
 import LocationMap from '@/components/homepage/homepageMap';
 import BusinessInfo from '@/components/homepage/about/BusinessInfo';
 import CarouselTestimonials from '@/components/homepage/about/testimonials/Review';
@@ -33,6 +34,7 @@ function Home() {
       <BackgroundBeams className="bg-black absolute inset-0 z-[-1]" />
       <AboutUs />
       </div>
+      <Slogan />
       <CarouselTestimonials />
       <div className="flex justify-center">
         <a href={reviewLink} target="_blank" rel="noopener noreferrer" className='w-11/12 md:w-1/3 bg-blue-500 text-white hover:bg-blue-700 transition-colors p-3 rounded-lg mt-2 md:mt-4 mb-4 md:mb-0 text-center'>Worked with us before? Let us know how we did!</a>
